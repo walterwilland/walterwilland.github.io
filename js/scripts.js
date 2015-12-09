46884b6b0f5e62f6524de376b9f18afe7462ce57
Walter Willand <wwilland@it-jpub-m2.eastern.ewu.edu>
2015-12-08 22:20:27 -0800

    Java Script & Code Clean Up

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// My Scripts
+
+// How to implement the lettering js plugin
+// $('element').lettering();
+
+// How to implement the feather light js plugin
+// $('element').featherlight(content, configuration);
+
+$(document).ready(function(){
+
+    $('h1').lettering();
+
+});
+
+
+//$('h1').featherlight();
